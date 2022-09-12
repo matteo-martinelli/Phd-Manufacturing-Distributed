05b277e6ea78e43028d2534d1de8de744abb69e6
WMex <52628801+wmex91@users.noreply.github.com>
2022-09-12 18:27:40 +0200

    Update run_me.bat
    Updated with the last necessary code.
    To add: uncomment lines when is done.

--- run_me.bat
+++ run_me.bat
@@ -1,1 +1,3 @@
-cd venv/Scripts && activate && cd ../../manufacturing_model &&running_model.py
+::cd venv/Scripts && activate && cd ../../manufacturing_model && running_model.py
+::cd../causal_model_colab && python -m notebook
+cd causal_model_colab && python -m notebook
