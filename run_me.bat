bf9cce23f3b7bad030802c70ec9a28f1c4f82c91
WMex <52628801+wmex91@users.noreply.github.com>
2022-09-13 15:55:26 +0200

    Update run_me.bat
    modified to run sequentially the sim and the causal_model generation

--- run_me.bat
+++ run_me.bat
@@ -1,3 +1,2 @@
-::cd venv/Scripts && activate && cd ../../manufacturing_model && running_model.py
-::cd../causal_model_colab && python -m notebook
-cd causal_model_colab && python -m notebook
+cd venv/Scripts && activate && cd ../../manufacturing_model && running_model.py
+cd../causal_model_colab && python -m notebook
