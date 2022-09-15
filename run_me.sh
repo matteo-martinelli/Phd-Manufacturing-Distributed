ee559d28c4320433e0e8aba44f91391c2c012e5b
WMex <52628801+wmex91@users.noreply.github.com>
2022-09-15 20:35:21 +0200

    Update run_me.sh
    Added merge_logs.py script to the flow.

--- run_me.sh
+++ run_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-. venv/bin/activate && cd manufacturing_model && python3 running_model.py
+# Launch the script from the project root folder.
+. venv/bin/activate && cd manufacturing_model && python running_model.py && python merge_logs.py
 cd ../causal_model && python -m notebook
