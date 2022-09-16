376ee1f760773e09cd07992ba580c0776913b9ff
WMex <52628801+wmex91@users.noreply.github.com>
2022-09-16 12:27:06 +0200

    Update run_me.bat
    bat file updated with merge_logs.py run.

--- run_me.bat
+++ run_me.bat
@@ -1,2 +1,2 @@
-cd venv/Scripts && activate && cd ../../manufacturing_model && running_model.py
+cd venv/Scripts && activate && cd ../../manufacturing_model && running_model.py && merge_logs.py
 cd../causal_model_colab && python -m notebook
